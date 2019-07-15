d75e8ebe7630ff4df7aac6a7ed0f94d67acd86d2
xuewenG <gexuewen@outlook.com>
2019-07-15 10:03:18 +0800

    完成添加商品接口

--- coomall-backend/coomall-web-merchant/src/main/java/cn/edu/hfut/coomall/web/merchant/controller/ProductController.java
+++ coomall-backend/coomall-web-merchant/src/main/java/cn/edu/hfut/coomall/web/merchant/controller/ProductController.java
@@ -1,5 +1,13 @@
 package cn.edu.hfut.coomall.web.merchant.controller;
 
+import cn.edu.hfut.coomall.entity.Message;
+import cn.edu.hfut.coomall.entity.Product;
+import cn.edu.hfut.coomall.service.ProductService;
+import cn.edu.hfut.coomall.util.ResultUtil;
+import cn.edu.hfut.coomall.web.merchant.bean.AddProductReqBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +18,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/product")
 public class ProductController {
+
+    @Autowired
+    ProductService productService;
+
+    @PostMapping("/add")
+    public Message addProduct(@RequestBody AddProductReqBean addProductReqBean) {
+
+        String name = addProductReqBean.getName();
+        Integer price = addProductReqBean.getPrice();
+        Integer quantity = addProductReqBean.getQuantity();
+        Integer category = addProductReqBean.getCategory();
+        String detail = addProductReqBean.getDetail();
+        Integer merchantID = addProductReqBean.getMerchantID();
+
+        Product product = new Product(name, price, quantity, category, detail, merchantID);
+        productService.saveProduct(product);
+
+        return ResultUtil.success();
+    }
 
 }
