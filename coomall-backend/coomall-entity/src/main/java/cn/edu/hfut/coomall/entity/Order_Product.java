fa5da7f5529b47f8de039c798d81b3b53f54c76c
zhenglishan <602868253@qq.com>
2019-07-16 09:50:25 +0800

    通过商品ID获取商家ID

---
+++ coomall-backend/coomall-entity/src/main/java/cn/edu/hfut/coomall/entity/Order_Product.java
@@ -0,0 +1,45 @@
+package cn.edu.hfut.coomall.entity;
+
+/**
+ * @author 郑力煽
+ * @date 2019/7/16
+ */
+public class Order_Product {
+
+    private Integer orderID;
+    private Integer productID;
+    private String style;
+    private Integer amount;
+
+    public Integer getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(Integer orderID) {
+        this.orderID = orderID;
+    }
+
+    public Integer getProductID() {
+        return productID;
+    }
+
+    public void setProductID(Integer productID) {
+        this.productID = productID;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+}
---
+++ coomall-backend/coomall-web-common/src/main/java/cn/edu/hfut/coomall/web/common/bean/GetMerchantIDRespBean.java
@@ -0,0 +1,13 @@
+package cn.edu.hfut.coomall.web.common.bean;
+
+public class GetMerchantIDRespBean {
+    private Integer merchantID;
+
+    public Integer getMerchantID() {
+        return merchantID;
+    }
+
+    public void setMerchantID(Integer merchantID) {
+        this.merchantID = merchantID;
+    }
+}
