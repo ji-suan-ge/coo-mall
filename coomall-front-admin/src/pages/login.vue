c7d208e8a75d76619ddc1a345c6626597c738ff5
WinsomeWind <33242408+WinsomeWind@users.noreply.github.com>
2019-07-17 00:30:38 +0800

    一些更新

---
+++ coomall-front-admin/src/pages/login.vue
@@ -0,0 +1,129 @@
+<template>
+  <el-container>
+    <el-header id="header">
+      <el-row>
+        <el-col :span="4" :push="11">
+          <img src="../assets/coo.png" />
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-row id="header_bottom_row"></el-row>
+    <el-main id="main">
+      <el-form :model="form" ref="form"
+               label-position="right" label-width="100px" :rules="rules" >
+        <el-row type="flex" justify="center">
+          <el-form-item label="手机号" prop="phone">
+            <el-input type="text" placeholder="请输入您的手机号"
+                      v-model="form.phone" class="input_width"></el-input>
+          </el-form-item>
+        </el-row>
+        <el-row id="input_interval"></el-row>
+        <el-row type="flex" justify="center">
+          <el-form-item label="密码"  prop="password">
+            <el-input type="password" placeholder="请输入您的密码"
+                      v-model="form.password" class="input_width"></el-input>
+          </el-form-item>
+        </el-row>
+        <el-row id="input_bottom" class="find">
+          <el-col :span="3" :push="10">
+            <router-link to="/findPassword">找回密码</router-link>
+          </el-col>
+        </el-row>
+        <el-row type="flex" justify="center">
+          <el-form-item>
+            <el-button type="primary" id="bt_submit"
+                       @click="onSubmit('form')" round>登录</el-button>
+          </el-form-item>
+        </el-row>
+      </el-form>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      form: {
+        phone: '',
+        password: ''
+      },
+      rules: {
+        phone: [
+          {required: true, message: '手机号不能为空', trigger: 'blur'},
+          {pattern: /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/, message: '请输入有效的手机号', trigger: 'blur'}
+        ],
+        password: [
+          {required: true, message: '密码不为空', trigger: 'change'},
+          {min: 8, max: 16, message: '长度在 8 到 16 个字符', trigger: 'change'}
+        ]
+      }
+    }
+  },
+  methods: {
+    onSubmit: function (formName) {
+      let that = this
+      that.$refs[formName].validate((valid) => {
+        if (valid) {
+        } else {
+          this.$message({
+            type: 'error',
+            message: '用户名或密码不符合规范',
+            center: true
+          })
+          return false
+        }
+      })
+      that.axios.post('/admin/login', {
+        phoneNumber: that.form.phone,
+        password: that.form.password
+      })
+        .then(function (response) {
+          if (response.data.msg === '请求成功') {
+            localStorage.setItem('admin', JSON.stringify(response.data.data))
+            localStorage.setItem('isLogin', 'yes')
+            that.$message.success('登陆成功！')
+            that.$router.push('/platform')
+          } else {
+            that.loginFailed()
+          }
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+    },
+    loginFailed () {
+      this.$message.error('用户名或密码错误，请重新输入')
+    }
+  }
+}
+</script>
+
+<style>
+  #header {
+    margin-top: 100px;
+    height: 200px;
+    text-align: center
+  }
+  #header_bottom_row {
+    height: 60px
+  }
+  .input_width {
+    width: 250px
+  }
+  #input_interval {
+    height: 35px
+  }
+  #input_bottom {
+    height: 30px;
+    margin-bottom: 10px;
+  }
+  #bt_submit {
+    margin-right: 44px;
+    width: 150px;
+  }
+  a {
+    text-decoration: none;
+  }
+</style>
