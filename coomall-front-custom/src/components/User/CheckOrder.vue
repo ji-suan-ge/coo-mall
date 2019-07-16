1859862afeba9b5927dbe843a449c0a32d90f7c0
zyh223 <prassiacaesar@163.com>
2019-07-17 03:07:42 +0800

    完成了客户界面-我的订单

--- coomall-front-custom/src/components/User/CheckOrder.vue
+++ coomall-front-custom/src/components/User/CheckOrder.vue
@@ -2,8 +2,20 @@
   <div>
     <el-table :data="orderFormInfo" max-height="100%"
               :fit="true" :row-style="rowStyle" id="productInfo" stripe>
-      <el-table-column prop="id"
+      <el-table-column prop="orderID"
                        class="table_row" label="订单编号"></el-table-column>
+      <el-table-column prop="productName"
+                       class="table_row" label="商品名称"></el-table-column>
+      <el-table-column prop="productStyle"
+                       class="table_row" label="规格分类"></el-table-column>
+      <el-table-column prop="address"
+                       class="table_row" label="收货地址"></el-table-column>
+      <el-table-column prop="price"
+                       class="table_row" label="单价"></el-table-column>
+      <el-table-column prop="amount"
+                       class="table_row" label="数量"></el-table-column>
+      <el-table-column prop="subTotal"
+                       class="table_row" label="小计"></el-table-column>
       <el-table-column prop="createTime"
                        class="table_row" label="创建时间"></el-table-column>
       <el-table-column prop="shipTime"
@@ -19,32 +31,32 @@
       <el-table-column class="table_row" label="操作">
         <template slot-scope="scope">
           <el-row  v-if="checkState(1)">
-            <el-col :span="12">
-              <el-button @click="purchaseHandler(scope.row.id)">付款</el-button>
+            <el-col>
+              <el-button @click="purchaseHandler(scope.row.orderID)">付款</el-button>
             </el-col>
-            <el-col :span="12">
-              <el-button @click="cancelHandler(scope.row.id)">取消</el-button>
+            <el-col>
+              <el-button @click="cancelHandler(scope.row.orderID)">取消</el-button>
+            </el-col>
+          </el-row>
+          <el-row  v-show="checkState(3)">
+            <el-col>
+              <el-button @click="signedHandler(scope.row.orderID)">签收</el-button>
+            </el-col>
+          </el-row>
+          <el-row  v-if="checkState(4)">
+            <el-col>
+              <el-button @click="completeHandler(scope.row.orderID)">完成</el-button>
+            </el-col>
+            <el-col>
+              <el-button @click="returnHandler(scope.row.orderID)">退货</el-button>
+            </el-col>
+          </el-row>
+          <el-row  v-if="checkState(6)">
+            <el-col>
+              <el-button @click="confirmReturnHandler(scope.row.orderID)">确认退货</el-button>
             </el-col>
           </el-row>
         </template>
-        <el-row  v-if="checkState(3)">
-          <el-col>
-            <el-button @click="signedHandler(scope.row.id)">签收</el-button>
-          </el-col>
-        </el-row>
-        <el-row  v-if="checkState(4)">
-        <el-col :span="12">
-          <el-button @click="completeHandler(scope.row.id)">完成</el-button>
-        </el-col>
-        <el-col :span="12">
-          <el-button @click="returnHandler(scope.row.id)">退货</el-button>
-        </el-col>
-      </el-row>
-        <el-row  v-if="checkState(6)">
-          <el-col>
-            <el-button @click="confirmReturnHandler(scope.row.id)">确认退货</el-button>
-          </el-col>
-        </el-row>
       </el-table-column>
     </el-table>
     <el-pagination
@@ -86,16 +98,16 @@ export default {
   watch: {
     state (val) {
       console.log('State of CheckOrder change to ' + this.state)
-      this.getAddressData(1)
+      this.getOrderData(1)
     }
   },
   created () {
     // console.log('navigate to CheckOrder with state of ' + this.$route.query.state)
     // this.state = this.$route.query.state
-    this.getAddressData(1)
+    this.getOrderData(1)
   },
   methods: {
-    getAddressData (cur) {
+    getOrderData (cur) {
       let that = this
       that.axios.post('/order/getByCustomIDAndState', {
         customID: JSON.parse(localStorage.getItem('custom')).id,
@@ -106,7 +118,10 @@ export default {
         .then(function (response) {
           if (response.data.msg === '请求成功') {
             that.total = parseInt(response.data.data.totalPage) * 20
-            that.orderFormInfo = response.data.data.orderList
+            that.orderFormInfo = response.data.data.orderReturnList
+            if (response.data.data.orderReturnList != null && response.data.data.orderReturnList[0] != null) {
+              that.orderFormInfo[0].subTotal = that.orderFormInfo[0].price * that.orderFormInfo[0].amount
+            }
           }
         })
         .catch(function (error) {
@@ -114,9 +129,12 @@ export default {
         })
     },
     changeDataHandler () {
-      this.getAddressData(this.currentPage)
+      this.getOrderData(this.currentPage)
     },
     checkState (check) {
+      // console.log(parseInt(this.state))
+      // console.log(parseInt(check))
+      // console.log(parseInt(this.state) === parseInt(check))
       return parseInt(this.state) === parseInt(check)
     },
     purchaseHandler (orderID) {
@@ -142,7 +160,7 @@ export default {
         orderID: orderID,
         state: 4
       }).then(res => {
-        console.log('Change order:' + orderID + ' to state ' + 0)
+        console.log('Change order:' + orderID + ' to state ' + 4)
         if (res.data.code === '0000') {
           this.$message({message: '订单已签收', type: 'success'})
         } else {
@@ -156,7 +174,7 @@ export default {
         orderID: orderID,
         state: 5
       }).then(res => {
-        console.log('Change order:' + orderID + ' to state ' + 0)
+        console.log('Change order:' + orderID + ' to state ' + 5)
         if (res.data.code === '0000') {
           this.$message({message: '订单已完成', type: 'success'})
         } else {
@@ -168,9 +186,9 @@ export default {
       let that = this
       that.axios.post('order/changeState', {
         orderID: orderID,
-        state: 5
+        state: 6
       }).then(res => {
-        console.log('Change order:' + orderID + ' to state ' + 0)
+        console.log('Change order:' + orderID + ' to state ' + 6)
         if (res.data.code === '0000') {
           this.$message({message: '退货申请成功', type: 'success'})
         } else {
@@ -182,9 +200,9 @@ export default {
       let that = this
       that.axios.post('order/changeState', {
         orderID: orderID,
-        state: 5
+        state: 7
       }).then(res => {
-        console.log('Change order:' + orderID + ' to state ' + 0)
+        console.log('Change order:' + orderID + ' to state ' + 7)
         if (res.data.code === '0000') {
           this.$message({message: '确认退货成功', type: 'success'})
         } else {
