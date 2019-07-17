dba45e844e0462aa4211c404dac2183f2b6ebe40
xuewenG <gexuewen@outlook.com>
2019-07-17 15:40:30 +0800

    12

--- coomall-front-merchant/src/components/orderForm/Returning.vue
+++ coomall-front-merchant/src/components/orderForm/Returning.vue
@@ -30,7 +30,7 @@
                        class="table_row" label="创建时间"></el-table-column>
       <el-table-column class="table_row" label="操作">
         <template slot-scope="scope">
-          <el-button type="text" @click="returnProduct(scope.$index, scope.row)">发货</el-button>
+          <el-button type="text" @click="returnProduct(scope.$index, scope.row)">退货</el-button>
         </template>
       </el-table-column>
     </el-table>
